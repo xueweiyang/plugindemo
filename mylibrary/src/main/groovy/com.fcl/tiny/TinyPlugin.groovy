994f430cee45629265e11a3f7a3c766b191779da
galio.fang <94fclove@gmail.com>
2018-12-07 19:13:33 +0800

    h

--- mylibrary/src/main/groovy/com.fcl/tiny/TinyPlugin.groovy
+++ mylibrary/src/main/groovy/com.fcl/tiny/TinyPlugin.groovy
@@ -1,3 +1,5 @@
+package com.fcl.tiny
+
 import com.android.build.gradle.AppPlugin
 import com.tinify.Tinify
 import org.gradle.api.Plugin
@@ -10,28 +12,37 @@ public class TinyPlugin implements Plugin<Project> {
         def hasApp = project.plugins.withType(AppPlugin)
         def variants = hasApp ? project.android.applicationVariants : project.android.libraryVariants
         def tinyInfo = project.extensions.create("tinyinfo", TinyInfo)
+
         project.afterEvaluate {
             variants.all { variant ->
-                def processResourceTask = project.task.findByName("process${variant.name.capitalize()}Resources")
+                def processResourceTask = project.tasks.findByName("process${variant.name.capitalize()}Resources")
                 def tinyPlugin = "tinyPlugin${variant.name.capitalize()}"
                 project.task(tinyPlugin) << {
-                    def apiKey = tinyInfo.apiKey
+                    //                    def apiKey = tinyInfo.apiKey
+                    def tinyFilePath = project.rootDir.path+"/tiny.txt"
+                    println("tinyfilepath:${tinyFilePath}")
+                    def tinyFile = new File(tinyFilePath)
+                    def apiKey = "PDII5xGelp6UwOeFZEoIevDdS8lSfGKF"
                     Tinify.setKey(apiKey)
                     Tinify.validate()
 
                     String resPath = "${project.projectDir}/src/main/res/"
                     def dir = new File(resPath)
-                    dir.eachDirMatch(~/mipmap[a-z-]*/) {
-                        def mipmapDir = new File(it)
-                        mipmapDir.eachFile { file->
-                            def fis = new FileInputStream(file)
-                            println("beforeSize:${fis.available()}")
-                            Tinify.fromFile(file).toFile(file)
-                            println("afterSize:${fis.available()}")
-                        }
-                    }
+//                    dir.eachDirMatch(~/mipmap-xhdpi/) {
+//
+//                        it.eachFile { file ->
+//                            println("tiny file:${file}")
+//                            def fis = new FileInputStream(file)
+//                            println("beforeSize:${fis.available()}")
+//                            def source = Tinify.fromFile(file.path)
+//                            println("get source")
+//                            source.toFile(file.path)
+//                            println("afterSize:${fis.available()}")
+//                        }
+//                    }
                 }
-                project.tasks.findByName(tinyPlugin).dependsOn processResourceTask.taskDependencies.getDependencies(processResourceTask)
+                project.tasks.findByName(tinyPlugin).dependsOn processResourceTask.taskDependencies.getDependencies(
+                    processResourceTask)
                 processResourceTask.dependsOn project.tasks.findByName(tinyPlugin)
             }
         }
